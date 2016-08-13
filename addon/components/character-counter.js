acd89d8a441f7efd2538b48ca568e36a5d05220f
nmaaruf <nabaz.maaruf@aenetworks.com>
2016-08-13 16:33:41 -0400

    added blueprint, added overlimit class when go over limit

--- addon/components/character-counter.js
+++ addon/components/character-counter.js
@@ -6,9 +6,16 @@ export default Ember.Component.extend({
   characterLimit: 0,
   characterCount: 0,
   inputClass: '',
+  isOverLimit: false,
   setup: Ember.on('didInsertElement', function() {
+    let inputLength = Ember.$('.' + this.inputClass + '').val().length;
     Ember.$('body').on('keyup', (e) => {
       this.set('characterCount', (this.characterLimit - Ember.$('.' + this.inputClass + '').val().length));
+      if(Ember.$('.' + this.inputClass + '').val().length > this.characterLimit){
+        this.set('isOverLimit', true);
+      }else{
+        this.set('isOverLimit', false);
+      }
     });
   }),
 });
